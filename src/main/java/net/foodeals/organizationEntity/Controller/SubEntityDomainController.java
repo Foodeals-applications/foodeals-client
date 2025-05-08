ef6a5630178965a95d864f580afef3aa5db3175f
hichembiessti <HNA24058@open-groupe.com>
2025-05-08 15:44:04 +0200

    update list domain by subentiy

--- src/main/java/net/foodeals/organizationEntity/Controller/SubEntityDomainController.java
+++ src/main/java/net/foodeals/organizationEntity/Controller/SubEntityDomainController.java
@@ -1,19 +1,18 @@
 package net.foodeals.organizationEntity.Controller;
 
-import lombok.RequiredArgsConstructor;
-import net.foodeals.organizationEntity.application.dtos.responses.SubEntityDomainResponse;
-import net.foodeals.organizationEntity.application.dtos.responses.SubEntityProductCategoryResponse;
-import net.foodeals.organizationEntity.application.services.SubEntityDomainService;
-import net.foodeals.organizationEntity.application.services.SubEntityProductCategoryService;
-import net.foodeals.organizationEntity.domain.entities.SubEntityDomain;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import net.foodeals.organizationEntity.application.dtos.responses.SubEntityDomainResponse;
+import net.foodeals.organizationEntity.application.dtos.responses.SubEntityProductCategoryResponse;
+import net.foodeals.organizationEntity.application.services.SubEntityDomainService;
 
 @RestController
 @RequestMapping("v1/subentities-domains")
@@ -34,4 +33,5 @@ public class SubEntityDomainController {
         List<SubEntityProductCategoryResponse> responses =subEntityDomainService.findSubEntityProductCategoriesBySubEntityDomain(id);
         return ResponseEntity.ok(responses);
     }
+    
 }
--- src/main/java/net/foodeals/organizationEntity/application/services/SubEntityDomainService.java
+++ src/main/java/net/foodeals/organizationEntity/application/services/SubEntityDomainService.java
@@ -1,15 +1,16 @@
 package net.foodeals.organizationEntity.application.services;
 
-import net.foodeals.organizationEntity.application.dtos.responses.SubEntityDomainResponse;
-import net.foodeals.organizationEntity.application.dtos.responses.SubEntityProductCategoryResponse;
-import net.foodeals.organizationEntity.domain.entities.SubEntityDomain;
-
 import java.util.List;
 import java.util.UUID;
+
+import net.foodeals.organizationEntity.application.dtos.responses.SubEntityDomainResponse;
+import net.foodeals.organizationEntity.application.dtos.responses.SubEntityProductCategoryResponse;
 
 public interface SubEntityDomainService {
 
     List<SubEntityDomainResponse> findAll();
 
     List<SubEntityProductCategoryResponse>findSubEntityProductCategoriesBySubEntityDomain(UUID id);
+    
+   
 }
