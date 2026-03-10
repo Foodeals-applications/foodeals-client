31f99d9b8d180876069361e21c2d1830b5c4fb5e
Anass Bentaib <banasstaib@gmail.com>
2026-03-10 23:01:04 +0000

    general fixes

--- src/main/java/net/foodeals/config/CorsConfig.java
+++ src/main/java/net/foodeals/config/CorsConfig.java
@@ -1,15 +1,21 @@
 package net.foodeals.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String[] origins = StringUtils.commaDelimitedListToStringArray(allowedOrigins);
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080") // Allow all origins
+                .allowedOrigins(origins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Access-Control-Allow-Origin").allowCredentials(true);
